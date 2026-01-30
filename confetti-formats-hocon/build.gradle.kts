cc94fc0fe66c22f90e126208e34943d7b973b3f4
QuickWrite <git@quickwrite.net>
2026-01-30 19:56:49 +0100

    Adding the necessary dependencies for hocon module
    The hocon module needs two different dependencies
    1. The core to integrate into all the other systems and to have a
       standardized implementation.
    2. The official HOCON parser from typesafe. This provides the
       implementation for managing the config.
    
    As the API is necessary to work with this library, it is added as an api
    in Gradle. The typedafe config library on the other hand is only wrapped
    and there is no need to access this API.

--- confetti-formats-hocon/build.gradle.kts
+++ confetti-formats-hocon/build.gradle.kts
@@ -3,6 +3,9 @@ plugins {
 }
 
 dependencies {
+    api(project(":confetti-core"))
+    implementation("com.typesafe:config:1.4.5")
+
     // Tests
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
