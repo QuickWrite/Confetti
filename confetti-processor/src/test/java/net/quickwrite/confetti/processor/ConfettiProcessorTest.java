94547b05e890119d90948752995f42488cee807a
QuickWrite <git@quickwrite.net>
2026-02-26 01:36:59 +0100

    Fix test for ConfettiProcessor
    The ConfettiProcessor has to be initialized to be able to work.
    This was not done, which meant that the test failed.

--- confetti-processor/src/test/java/net/quickwrite/confetti/processor/ConfettiProcessorTest.java
+++ confetti-processor/src/test/java/net/quickwrite/confetti/processor/ConfettiProcessorTest.java
@@ -11,16 +11,50 @@ import net.quickwrite.confetti.resolver.ConfettiTypeResolver;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.annotation.processing.Filer;
+import javax.annotation.processing.Messager;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
+import java.util.Locale;
+import java.util.Map;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ConfettiProcessorTest {
+    // Stub of a ProcessingEnvironment that does nothing.
+    private static class SimpleProcessingEnvironment implements ProcessingEnvironment {
+        @Override
+        public Map<String, String> getOptions() { return Map.of(); }
+
+        @Override
+        public Messager getMessager() { return null; }
+
+        @Override
+        public Filer getFiler() { return null; }
+
+        @Override
+        public Elements getElementUtils() { return null; }
+
+        @Override
+        public Types getTypeUtils() { return null; }
+
+        @Override
+        public SourceVersion getSourceVersion() { return null; }
+
+        @Override
+        public Locale getLocale() { return null; }
+    }
+
     private ConfettiProcessor confettiProcessor;
 
     @BeforeEach
     public void setUp() {
         this.confettiProcessor = new ConfettiProcessor();
+
+        this.confettiProcessor.init(new SimpleProcessingEnvironment());
     }
 
     @Test
